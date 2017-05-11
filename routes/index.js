5b1d09631fc1f6442b3fbb4c49cde46e8f1aa203
Yassir Akram <yassir.akram@hotmail.fr>
2017-05-12 00:18:42 +0200

    Initial Commit

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,17 @@ app.get('/', function(req, res, next) {
   res.render('index');
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VERIFY_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
 app.get('/weather', function(req, res) {
   var req = weatherService.getGeolocalisation("Palaiseau");
   req.then(function(htmlStr) {
